1877f6080666ef1f1b35cf1ac1dbe1b73faf1238
Luis Yoshida <luis.yoshida@gmail.com>
2015-06-10 20:31:17 -0300

    SentMemeViewController added. Interface changes.

---
+++ Meme Me/SentMemeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SentMemeViewController.swift
+//  Meme Me
+//
+//  Created by Luis Yoshida on 6/10/15.
+//  Copyright (c) 2015 Msen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SentMemeViewController : UIViewController {
+    
+}
